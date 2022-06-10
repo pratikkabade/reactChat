a1bc7782f7910917b25bef6be37dd538785bfe89
Pratik Kabade <76637730+pratikkabade@users.noreply.github.com>
2022-06-10 10:58:44 +0530

    Updates

---
+++ precache-manifest.9ae0e7f14b027a6b57be6e569b7363bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7973dd71506fcfff7c770279c82d4c",
+    "url": "/reactChat/index.html"
+  },
+  {
+    "revision": "809cea11084343c196c4",
+    "url": "/reactChat/static/css/main.d5c0ca3d.chunk.css"
+  },
+  {
+    "revision": "adb0a54538eafced32d2",
+    "url": "/reactChat/static/js/2.111e7743.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/reactChat/static/js/2.111e7743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809cea11084343c196c4",
+    "url": "/reactChat/static/js/main.39d522d8.chunk.js"
+  },
+  {
+    "revision": "ac46e041e672f3afbded",
+    "url": "/reactChat/static/js/runtime-main.3649e7d4.js"
+  }
+]);
